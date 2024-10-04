e8ae206ae1c2729ab05302c01daf6514b78455a1
yellowpeanut <yellowpeanut24@rambler.ru>
2024-10-04 20:00:47 +0300

    initail create

---
+++ fc-test-task.tests/InitialData/User/InitialUsers.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace FcTestTask.Tests.InitialData.User;
+
+public class InitialUsers
+{
+    public static IEnumerable<fc_test_task.User> GetList()
+    {
+        var json = File.ReadAllText(Environment.CurrentDirectory + "../../../../" + 
+            "./InitialData/User/users.json");
+        var data = JsonSerializer.Deserialize<List<fc_test_task.User>>(json);
+        if(data == null)
+        {
+            throw new Exception("File users.json is not valid");
+        }
+
+        return data;
+    }
+}

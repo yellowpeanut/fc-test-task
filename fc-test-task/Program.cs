1fa018eeec220b2e6deb0185b62ff0ebc8da3c06
yellowpeanut <yellowpeanut24@rambler.ru>
2024-10-07 14:56:06 +0300

    apply migration before start

--- fc-test-task/Program.cs
+++ fc-test-task/Program.cs
@@ -43,4 +43,9 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+    db.Database.Migrate();
+}
 app.Run();
